32d8b44917358c415f3f46f0ee4b2a6cb76e27f8
Nain <justin@hnain.dev>
2022-10-17 13:39:58 -0700

    Convert string to number

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,15 +1,15 @@
 const ftoc = function(fahr) {
-  let convertedNum = ((fahr - 32)*(5/9)).toFixed(1);
+  let convertedNum = Number(((fahr - 32)*(5/9)).toFixed(1));
 
-  if (convertedNum === Math.floor(convertedNum)) return parseInt(convertedNum);
+  if (convertedNum === Math.floor(convertedNum)) return Math.trunc(convertedNum);
   else return convertedNum;
 
 };
 
 const ctof = function(cel) {
-  let convertedNum = ((9/5)*cel + 32).toFixed(1);
+  let convertedNum = Number((9/5)*cel + 32).toFixed(1));
 
-  if (convertedNum === Math.floor(convertedNum)) return parseInt(convertedNum);
+  if (convertedNum === Math.floor(convertedNum)) return Math.trunc(convertedNum);
   else return convertedNum;
 
 };
