d953f3e6e3125a971c7d5e4c2f750a519192fd4b
Nain <justin@hnain.dev>
2022-10-17 13:24:18 -0700

    Implement simple exercise fns

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
+const ftoc = function(cel) {
+
+  return (32 + (9/5)*cel).toFixed(1);
 
 };
 
-const ctof = function() {
-
+const ctof = function(fahr) {
+  return (5/9)*(fahr - 32).toFixed(1);
 };
 
 // Do not edit below this line
