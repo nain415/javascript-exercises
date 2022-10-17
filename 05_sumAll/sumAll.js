e99731ffcc4728680814c7a16b4e4b6021d56ae9
Nain <justin@hnain.dev>
2022-10-17 12:56:25 -0700

    Identify smallest int

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,13 @@
 const sumAll = function(int1,int2) {
-    let biggestInt = int1;
-    
-    if (int1 < int2) {
-        biggestInt = int2;
-    }
+    let smallestInt = int1;
+    let biggestInt = int2;
+
+    if (int1 > int2) {
+        biggestInt = int1;
+        smallestInt = int2;
+    };
+
+    // for (let i = biggestInt, )
 
 };
 
