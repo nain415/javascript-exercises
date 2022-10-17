e615af803531b67f32bd8d053c9b9eaaded6827c
Nain <justin@hnain.dev>
2022-10-17 13:28:05 -0700

    Correct conversion functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,9 @@
-const ftoc = function(cel) {
-
-  return (32 + (9/5)*cel).toFixed(1);
-
+const ftoc = function(fahr) {
+  return ((fahr - 32)*(5/9)*cel).toFixed(1);
 };
 
-const ctof = function(fahr) {
-  return (5/9)*(fahr - 32).toFixed(1);
+const ctof = function(cel) {
+  return ((9/5)*fahr + 32).toFixed(1);
 };
 
 // Do not edit below this line
