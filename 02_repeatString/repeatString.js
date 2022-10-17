617f354eeb8c1e8913978cbe997e8509b4091143
Nain <justin@hnain.dev>
2022-10-17 12:21:36 -0700

    Add while loop and append var

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
 const repeatString = function(str,num) {
 
+    append = str;
+
+    while (num > 1) {
+        str += append;
+        num--;
+    };
+
+    return str;
+
 };
 
 // Do not edit below this line
