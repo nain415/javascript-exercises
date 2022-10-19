b6f38a969e6c22a8f580a626e7e4da3c4e12c7d8
Nain <justin@hnain.dev>
2022-10-19 15:41:01 -0700

    Complete Palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    str = str.split('').filter(char => (char >= 'A' && char <= 'z')).join(''); //removes punctuation
+    return  str === str.split('').reverse().join('').toLowerCase(); //reverses the string
 };
 
 // Do not edit below this line
