2d6c08e6e9d9c74350e51b62c3bd24837db6241f
Nain <justin@hnain.dev>
2022-10-19 15:27:37 -0700

    Complete calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((accum, num) => accum += num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accum, num) => accum *= num, 1);
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	return num**pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let finalcount = 1;
+  for (let i = 1; i < num; i++) {
+    finalcount *= i+1;
+  };
+  return finalcount;
 };
 
 // Do not edit below this line
